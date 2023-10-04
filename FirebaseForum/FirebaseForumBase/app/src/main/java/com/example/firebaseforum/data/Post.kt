14a0fec091514549c72aa506d7d2b218e9811ab1
Mikołaj Strzelczyk <90414474+Strzelba72@users.noreply.github.com>
2023-10-04 20:19:21 +0200

    commit

--- FirebaseForum/FirebaseForumBase/app/src/main/java/com/example/firebaseforum/data/Post.kt
+++
@@ -1,9 +0,0 @@
-package com.example.firebaseforum.data
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-
-@Parcelize
-data class Post(val author:String? = null, val message:String? = null, val timestamp: Long? =
-    null): Parcelable
--- FirebaseForum/FirebaseForumBase/app/src/main/java/com/example/firebaseforum/data/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.firebaseforum.data
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class User(val email:String? = null) : Parcelable
